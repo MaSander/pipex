7fb63f3d3bfa3dbeabea4e7d3e3e399029ec6ab9
MaSander <ma.sander11@gmail.com>
2023-03-21 20:33:15 -0300

    multiple commands worked

--- pipex.c
+++ pipex.c
@@ -3,74 +3,82 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msaner- <msander-@student.42sp.org.br>     +#+  +:+       +#+        */
+/*   By: msander <msander@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 22:28:20 by msaner-           #+#    #+#             */
-/*   Updated: 2022/11/27 07:10:18 by msaner-          ###   ########.fr       */
+/*   Updated: 2023/03/21 20:32:07 by msander          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	execute(char *argv, char **env)
+char **g_env;
+
+void	execute(char *argv)
 {
 	char	*path;
 	char	**cmd_arg;
 
-	cmd_arg = ft_split_ignore(argv, ' ', '\'');
-	path = find_path(cmd_arg[0], env);
+	cmd_arg = ft_split(argv, '|');
+	path = find_path(cmd_arg[0], g_env);
 	if (!path)
 	{
 		ft_free_array(cmd_arg);
 		invalid_command();
 	}
-	execve(path, cmd_arg, env);
+	execve(path, cmd_arg, g_env);
 }
 
-void	execute_in(char **argv, int *pipe_fd, char **env)
+void	execute_in(char **argv, int count_cmd, int *pipe_fd)
 {
-	int	fd_file_in;
-
 	close(pipe_fd[0]);
-	fd_file_in = open(argv[1], O_RDONLY);
-	if (fd_file_in == -1)
-		exit(0);
-	dup2(fd_file_in, 0);
 	dup2(pipe_fd[1], 1);
-	execute(argv[2], env);
+	execute(argv[count_cmd]);
 }
 
-void	execute_out(char **argv, int *pipe_fd, char **env)
+void execute_on_pipe(char **argv, int count_cmd, int *pipe_fd)
 {
-	int	fd_file_out;
-
-	close(pipe_fd[1]);
-	fd_file_out = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 00664);
-	if (fd_file_out == -1)
-		exit (1);
-	dup2(fd_file_out, 1);
 	dup2(pipe_fd[0], 0);
-	execute(argv[3], env);
+	dup2(pipe_fd[1], 1);
+	execute(argv[count_cmd]);
+}
+
+void	execute_out(char **argv, int count_cmd, int *pipe_fd)
+{
+	close(pipe_fd[1]);
+	dup2(pipe_fd[0], 0);
+	dup2(1, 1);
+	execute(argv[count_cmd]);
 }
 
 int	main(int argc, char *argv[], char **env)
 {
 	int		pipe_fd[2];
 	pid_t	pid;
+	int 	count_cmd;
 
-	if (argc != 5)
-		print_error_fd("\033[31mError: Bad arguments\n\e[0m", 1);
-	if (pipe(pipe_fd) == -1)
-		print_error_fd("\033[31mError: Internal Error. pipe()\n\e[0m", 1);
-	pid = fork();
-	if (pid == -1)
-		print_error_fd("\033[31mError: Internal Error. fork()\n\e[0m", 1);
-	if (pid == 0)
-		execute_in(argv, pipe_fd, env);
-	else
+	g_env = env;
+	count_cmd = 1;
+
+	while(argc != count_cmd)
 	{
-		waitpid(pid, NULL, 0);
-		execute_out(argv, pipe_fd, env);
+		pid = fork();
+		if (pid == -1)
+			print_error_fd("\033[31mError: Internal Error. fork()\n\e[0m", 1);
+		if(pid == 0)
+		{
+			if (count_cmd == 1)
+				execute_in(argv, count_cmd, pipe_fd);
+			else
+				execute_on_pipe(argv, count_cmd, pipe_fd);
+		}
+		else
+		{
+			if(count_cmd + 1 == argc)
+				execute_out(argv, count_cmd, pipe_fd);
+			waitpid(pid, NULL, 0);
+		}
+		count_cmd++;
 	}
 	return (0);
 }
